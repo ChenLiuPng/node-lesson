c9004242f8e831dbd192fe9c354e9d96a845dad8
陈柳鹏 <vclp@163.com>
2021-10-31 06:57:26 +0800

    feat: promise宏任务微任务概念 新建node

---
+++ 4.review/1.js
@@ -0,0 +1,15 @@
+// 常见的宏任务
+// setTimeout(() => {
+    
+// }, timeout);
+// setImmediate
+// MessageChannel
+// requestAnimationFrame
+// script ui i/o 事件 ajax
+
+// 常见的微任务
+
+// MutationObserver
+// Promise.then
+// process.nextTick
+
---
+++ 5.node/1.node.js
@@ -0,0 +1,5 @@
+// 概念 node是什么 不是一门语言 核心就是一个让js可以运行服务端的一个运行时
+
+// js 有几部分组成（DOM BOM） ECMASCRIPT
+
+// node中只能使用ECMASCRIPT 语法本身
